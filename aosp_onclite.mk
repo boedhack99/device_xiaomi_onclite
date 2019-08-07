d2cc2ab50928f345422f2e6a40ac6875f67dfede
ErrorNetwork28 <zidan23.05.2016@gmail.com>
2019-08-07 10:50:19 +0000

    onclite: Include Pixel Experience configs

---
+++ aosp_onclite.mk
@@ -0,0 +1,42 @@
+# Copyright (C) 2019 The PixelExperience Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/xiaomi/onclite/device.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_onclite
+PRODUCT_DEVICE := onclite
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 7
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "Xiaomi/onclite/onclite:9/PKQ1.181021.001/V10.2.8.0.PFLEUXM:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="onclite-user 9 PKQ1.181021.001 V10.2.8.0.PFLEUXM release-keys" \
+    PRODUCT_NAME="onclite" \
+    TARGET_DEVICE="onclite"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
