c6f7cd1a2c8d757a89fec991cb85e7f9465c52e8
ErrorNetwork28 <zidan23.05.2016@gmail.com>
2019-08-14 19:05:03 +0000

    onclite: Change defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,11 +56,11 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_CONFIG := onclite-perf_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm632
+TARGET_KERNEL_CONFIG := msm8953-perf_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_PATH := $(PWD)/vendor/qcom/sdclang/6.0/prebuilt/linux-x86_64/bin
+TARGET_KERNEL_CLANG_COMPILE := false
+TARGET_KERNEL_CLANG_VERSION := 9.0.5
 TARGET_KERNEL_VERSION := 4.9
 
 # Audio
