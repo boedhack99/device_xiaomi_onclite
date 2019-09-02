5aee0dc8894753b54c50532a23952aa031209564
Dhina17 <dhinalogu@gmail.com>
2019-09-02 15:08:48 +0000

    Revert "onclite: Inclute Google Camera"
    This reverts commit bbdac601f807befc5b2ac0466ebd5c664dc86cb2.

--- aosp_onclite.mk
+++ aosp_onclite.mk
@@ -18,7 +18,8 @@ $(call inherit-product, device/xiaomi/onclite/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-TARGET_BOOT_ANIMATION_RES := 720
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 
 # Inherit some common PixelExperience stuff.
--- device.mk
+++ device.mk
@@ -151,7 +151,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
-    GoogleCamera
+    Snap
 
 # Configstore
 PRODUCT_PACKAGES += \
