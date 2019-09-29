2d8f2f98257b97bdae6ee1e7fd5aaca9b6c791d4
KostyaJRZ <54120489+KostyaJRZ@users.noreply.github.com>
2019-09-29 17:49:50 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,10 +56,10 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-#TARGET_KERNEL_CONFIG := onclite-perf_defconfig
-#TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm632
-#TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-#TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := onclite-perf_defconfig#
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm632
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CLANG_COMPILE := true
 
 # Audio
 AUDIO_FEATURE_ENABLED_ANC_HEADSET := true
