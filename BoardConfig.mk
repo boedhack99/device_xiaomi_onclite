946fef3215bc2993a2434f4cf59d3e72ed1017ba
KostyaJRZ <54120489+KostyaJRZ@users.noreply.github.com>
2019-09-29 17:05:55 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_CONFIG := onclite-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm632
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CLANG_COMPILE := true
 
 # Audio
 AUDIO_FEATURE_ENABLED_ANC_HEADSET := true
