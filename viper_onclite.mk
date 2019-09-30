1d280585bc458c7ac15a218b31d43f0051cb17ba
KostyaJRZ <54120489+KostyaJRZ@users.noreply.github.com>
2019-09-30 23:14:47 +0300

    Update viper_onclite.mk

--- viper_onclite.mk
+++ viper_onclite.mk
@@ -26,7 +26,7 @@ TARGET_GAPPS_ARCH := arm64
 $(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := viper_onclite
+PRODUCT_NAME := lineage_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
